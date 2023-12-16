d2b0cc2697898690a433c20c5e6699a7f02c9646
Sagar <sagarpuniyani@gmail.com>
2023-12-16 23:13:00 +0530

    Distance are Calculated

--- KMeans/KMeans.py
+++ KMeans/KMeans.py
@@ -1,7 +1,34 @@
+import random 
+import numpy as np 
 class KMeans:
     def __init__(self , n_cluster = 2 , max_iter = 100) :
         self.n_cluster = n_cluster
         self.max_iter = max_iter
         self.centroid = None
 
-    
+    def fit_predict(self , Dataset ):
+        random_point = random.sample(range(0 , Dataset.shape[0]) , self.n_cluster)
+        self.centroid = Dataset[random_point]
+        print( self.centroid )
+
+        for i in range(self.max_iter):
+            # assign Cluster 
+            cluster_group = self.assign_cluster(Dataset)
+
+            # move centroid 
+            # check finish   
+
+    def assign_cluster(self , Dataset ) :
+        print("Enter")
+        cluster_group = []
+        distances = []
+
+        for row in Dataset:
+            distance = []
+            for centroid in self.centroid:
+                distance.append(np.sqrt(np.dot(row-centroid , row-centroid)))
+                distances.append(distance)
+        print("Distances")
+        print(distances)
+
+        return distances
--- KMeans/main.py
+++ KMeans/main.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
+from KMeans import KMeans
 
 centroid = [(4,4) , (-4,4) , (4,-4) , (-4,-4)]
 cluster_std = [1,2,1,1]
@@ -10,5 +11,12 @@ X,y = make_blobs(n_samples=100,
                 n_features=3,
                 random_state=2)
 
-plt.scatter(X[:,0] , X[:,1])
-plt.show()
+print( "X = " , X)
+
+km = KMeans(n_cluster = 2 , max_iter = 100 )
+km.fit_predict(X)
+
+
+
+# plt.scatter(X[:,0] , X[:,1])
+# plt.show()
